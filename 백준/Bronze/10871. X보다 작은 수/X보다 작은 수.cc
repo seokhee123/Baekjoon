248514f156b3a43766674aab6bd0d89f08e40243
seokhee123 <82377493+seokhee123@users.noreply.github.com>
2022-06-26 18:27:40 +0900

    [Bronze V] Title: X보다 작은 수, Time: 404 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, x;
+	cin >> n >> x;
+	int *arr = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		if (arr[i] < x) {
+			cout << arr[i] << " ";
+		}
+	}
+
+}
